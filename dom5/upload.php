ea5e038295a19b79a562d7addbd98b670f758238
pufnooty <40949535+pufnooty@users.noreply.github.com>
2018-07-10 16:47:52 +0300

    вв

--- dom5/upload.php
+++ dom5/upload.php
@@ -1,9 +1,17 @@
 <?php
-//echo $_FILES['nextimage']['type'];
+
+if (session_id() == '') {
+    session_start();
+}
+
+
 include (__DIR__.'/functions.php');
+
 If (null != getCurrentUser()){
 
     if (isset($_FILES['nextimage'])) {
+
+        $fh =  fopen(__DIR__.'/upload.log', 'a');
 
         If (('image/jpeg' == $_FILES['nextimage']['type']) || ('image/png' == $_FILES['nextimage']['type'])){
 
@@ -11,6 +19,7 @@ If (null != getCurrentUser()){
             If (0 == $_FILES['nextimage']['error']) {
 
                 move_uploaded_file($_FILES['nextimage']['tmp_name'], __DIR__ . '/gallery/' .$_FILES['nextimage']['name']);
+                fwrite($fh, getCurrentUser() . ';' . date('c') . ';' . $_FILES['nextimage']['name'] . "\r\n");
             }
         }
         else{
@@ -18,12 +27,12 @@ If (null != getCurrentUser()){
         }
     }
 ?>
-Изображение успешно выгружено! <a href="/dom5/index.php">назад</a>
+Изображение успешно загружено! <a href="/dom5/index.php">назад</a>
 <?php
 
 exit;
 }else{
     ?>
-    Пользователь не авторизован! <a href="/dom5/login.php">Перейти на страницу авторизации - login.php</a>
+    Пользователь не авторизован! <a href="login.php">Перейти на страницу авторизации - login.php</a>
     <?php
 }
